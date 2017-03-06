e3aa91ba63de9fc1aac86159815e90e949ffb672
Hitman007 <support@customrayguns.com>
2017-03-06 08:41:48 -0800

    Update iTekMods.php

--- iTekMods.php
+++ iTekMods.php
@@ -16,6 +16,7 @@ function recomveCopyrightInfo(){
 
 $output = <<<OUTPUT
 <script>
+	alert('JS active');
         jQuery("document").ready(function(){
 		jQuery(".footer-copyright-wrapper").html('<span style="float:left;">Copyright <a href = "http://jimsbigbrain.com">&copy;</a> Jim\'s Big Brain. All rights reserved. <a href = "http://customrayguns.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://customrayguns.com/wp-content/uploads/2012/08/ray_gun.gif" /></a>.</span>');
 	});
