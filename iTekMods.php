fa3a46c11c48f8e1ec0fe3fbeaa89f26f7a4ebb5
Hitman007 <support@customrayguns.com>
2017-03-20 18:58:36 -0700

    Update iTekMods.php

--- iTekMods.php
+++ iTekMods.php
@@ -18,6 +18,7 @@ $output = <<<OUTPUT
 <script>
         jQuery("document").ready(function(){
 		jQuery(".site-info").html('<span style="float:left;">Copyright <a href = "http://customrayguns.com/login/">&copy;</a> Custom Ray Guns. All rights reserved. <a href = "http://customrayguns.com/contact/">Contact us.</a> This site is a <a href = "http://customrayguns.com">Custom Ray Gun.<img align = "MIDDLE" src = "http://customrayguns.com/wp-content/uploads/ray_gun.png" /></a>.</span>');
+		jQuery(".brand").html('<span>Customer Ray Guns</span>');
 	});
 </script>
 <style>
